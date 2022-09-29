974f4986c752a636d03a718d2f6081bde4b79727
David Yao <davidyao2011@hotmail.com>
2022-09-30 00:29:44 +1300

    Added router to navigate pages and css file

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,18 @@
 import "./App.css";
+import { BrowerRouter, Route, Routes } from "react-router-dom";
+import Comments from "./components/Comments";
+import Task from "./components/Task";
+import Login from "./components/Login";
 
 function App() {
   return (
-    <div>
-      <p>Hello World!</p>
-    </div>
+    <BrowerRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/task" element={<Task />} />
+        <Route path="/comments/:category/:id" element={<Comments />} />
+      </Routes>
+    </BrowerRouter>
   );
 }
 
