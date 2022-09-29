282cee005590b6c0423826ef85872ed8b4ca4661
David Yao <davidyao2011@hotmail.com>
2022-09-30 09:23:37 +1300

    created Nav component

---
+++ client/src/components/Nav.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Nav() {
+  return (
+    <div>
+      <nav className="navbar">
+        <h3>Jobs's todo list</h3>
+      </nav>
+    </div>
+  );
+}
+
+export default Nav;
