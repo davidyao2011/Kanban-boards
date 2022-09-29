67d79d8bcaf735c3cf45e582d6c92314719400fc
David Yao <davidyao2011@hotmail.com>
2022-09-30 00:18:51 +1300

    Created socket connection to the server

---
+++ server/index.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const app = express();
+const PORT = 4000;
+
+app.use(express.urlencoded({ extened: true }));
+app.use(express.json());
+
+const http = require("http");
+const cors = require("cors");
+
+app.use(cors());
+
+const socketIO = require("socket.io")(http, {
+  cors: {
+    origin: "https://localhost:3000",
+  },
+});
+
+socketIO.on("connection", (socket) => {
+  console.log(`connected: ${socket.id} user just connected!`);
+  socket.on("disconnect", () => {
+    socket.disconnect();
+    console.log(`disconected: A user disconnected!`);
+  });
+});
+
+app.get("/api", (req, res) => {
+  res.json({
+    message: "Hello world!",
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on ${PORT}`);
+});
