5b12546e0199f87391445cd9141a3983c36a7e4b
David Yao <davidyao2011@hotmail.com>
2022-10-03 23:03:13 +1300

    Added todo image to the Nav component

--- client/src/components/Nav.js
+++ client/src/components/Nav.js
@@ -4,7 +4,17 @@ function Nav({ name }) {
   return (
     <div>
       <nav className="navbar">
-        <h3>`${name}'s todo list`</h3>
+        <h1>`${name}'s todo list`</h1>
+        <div>
+          <h3>What a beautiful day!</h3>
+        </div>
+        <div>
+          <img
+            className="img-todo"
+            src="https://images.unsplash.com/photo-1664142638093-9a78da96c425?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80"
+            alt="todo img"
+          />
+        </div>
       </nav>
     </div>
   );
